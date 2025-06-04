44b91751066a25d3de26e3275d02dd5914a76970
mbranches <marcusvbranches@gmail.com>
2025-06-04 12:21:04 -0300

    feat: adiciona classes modelo

---
+++ src/main/java/dev/branches/mode/Product.java
@@ -0,0 +1,20 @@
+package dev.branches.mode;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@EqualsAndHashCode(of = "id")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private Double unitPrice;
+}
---
+++ src/main/java/dev/branches/mode/Sale.java
@@ -0,0 +1,22 @@
+package dev.branches.mode;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Sale {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(optional = false)
+    private Product product;
+    @Column(nullable = false)
+    private Integer quantity;
+    @Column(nullable = false)
+    private Double totalValue;
+}
