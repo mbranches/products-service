0aed8e39381a8309693a21b31760e2d200c6f621
mbranches <marcusvbranches@gmail.com>
2025-06-04 16:03:37 -0300

    feat: adiciona builder para UserRole

--- src/main/java/dev/branches/model/UserRole.java
+++ src/main/java/dev/branches/model/UserRole.java
@@ -1,10 +1,16 @@
 package dev.branches.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @Entity(name = "user_role")
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserRole {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
