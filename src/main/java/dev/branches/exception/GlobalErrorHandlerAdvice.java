80881b9594c084865b550cf04b76a04490b2fc96
mbranches <marcusvbranches@gmail.com>
2025-06-04 19:15:02 -0300

    feat: adiciona tratamento de exceção para BadCredentialsException

--- src/main/java/dev/branches/exception/GlobalErrorHandlerAdvice.java
+++ src/main/java/dev/branches/exception/GlobalErrorHandlerAdvice.java
@@ -1,6 +1,8 @@
 package dev.branches.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -18,5 +20,12 @@ public class GlobalErrorHandlerAdvice {
         DefaultMessageError error = new DefaultMessageError(e.getStatusCode().value(), e.getReason());
 
         return ResponseEntity.status(e.getStatusCode()).body(error);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<DefaultMessageError> handlerBadCredentialsException(BadCredentialsException e) {
+        DefaultMessageError error = new DefaultMessageError(HttpStatus.UNAUTHORIZED.value(), "User or password invalid");
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
     }
 }
