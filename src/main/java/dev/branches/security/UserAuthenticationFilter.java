2b3d66eee2035c5087f4a33c56e40857213cfd4c
mbranches <marcusvbranches@gmail.com>
2025-06-04 16:03:10 -0300

    feat: adiciona UserAuthenticationFilter

---
+++ src/main/java/dev/branches/security/UserAuthenticationFilter.java
@@ -0,0 +1,50 @@
+package dev.branches.security;
+
+import dev.branches.model.User;
+import dev.branches.repository.UserRepository;
+import dev.branches.service.JwtTokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class UserAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtTokenService jwtTokenService;
+    private final UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = recoveryToken(request);
+
+        if (token != null) {
+            String subject = jwtTokenService.validateToken(token);
+
+            User user = userRepository.findByLogin(subject).get();
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(user.getUsername(), null, user.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoveryToken(HttpServletRequest request) {
+        String authentication = request.getHeader("Authorization");
+        if (authentication != null) {
+            return authentication.replace("Bearer ", "");
+        }
+
+        return null;
+    }
+}
