e6ea31c3ae4f5cb521d496102e56f1083d98ddd9
mbranches <marcusvbranches@gmail.com>
2025-06-04 18:59:52 -0300

    refactor: substitui ResponseStatusException por NotFoundException

--- src/main/java/dev/branches/service/ProductService.java
+++ src/main/java/dev/branches/service/ProductService.java
@@ -3,12 +3,11 @@ package dev.branches.service;
 import dev.branches.dto.ProductGetResponse;
 import dev.branches.dto.ProductPostRequest;
 import dev.branches.dto.ProductPostResponse;
+import dev.branches.exception.NotFoundException;
 import dev.branches.model.Product;
 import dev.branches.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -27,7 +26,7 @@ public class ProductService {
 
     public Product findByIdOrThrowsNotFoundException(Long id) {
         return repository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product with id \"%s\" not found"));
+                .orElseThrow(() -> new NotFoundException("Product with id '%s' not found".formatted(id)));
     }
 
     public ProductPostResponse create(ProductPostRequest postRequest) {
