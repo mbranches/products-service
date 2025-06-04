42fad7302c6bfab210fd9d68d1c528324599a632
mbranches <marcusvbranches@gmail.com>
2025-06-04 14:11:26 -0300

    docs: adiciona ao schema criação das tabelas user, role e user_role

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -11,3 +11,26 @@ CREATE TABLE IF NOT EXISTS sale (
     product_id bigint NOT NULL,
     FOREIGN KEY (product_id) REFERENCES product(id)
 );
+
+CREATE TABLE IF NOT EXISTS role (
+    id bigint PRIMARY KEY AUTO_INCREMENT,
+    name varchar(50) NOT NULL,
+    description varchar(50)
+);
+
+CREATE TABLE IF NOT EXISTS user (
+    id varchar(36) PRIMARY KEY,
+    login varchar(50) NOT NULL,
+    password varchar(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS user_role (
+    id bigint PRIMARY KEY AUTO_INCREMENT,
+    user_id VARCHAR(36) NOT NULL,
+    role_id bigint NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (role_id) REFERENCES role(id)
+);
+
+INSERT IGNORE INTO role(id, name, description) VALUES (1, 'admin', 'has all system accesses');
+INSERT IGNORE INTO role(id, name, description) VALUES (2, 'basic', 'has limited system accesses');
