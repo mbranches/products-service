3014a4b535b18658af181e61ce8a4d759c1f69c9
mbranches <marcusvbranches@gmail.com>
2025-06-05 13:43:14 -0300

    docs: altera entidade sale para agora ter um user associado e altera o relacionamento sale-product para n:n

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,17 +1,3 @@
-CREATE TABLE IF NOT EXISTS product (
-   id bigint PRIMARY KEY AUTO_INCREMENT,
-   name varchar(50) NOT NULL,
-   unit_price decimal(10, 2) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS sale (
-    id bigint PRIMARY KEY AUTO_INCREMENT,
-    quantity int NOT NULL,
-    total_value decimal(10, 2) NOT NULL,
-    product_id bigint NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES product(id)
-);
-
 CREATE TABLE IF NOT EXISTS role (
     id bigint PRIMARY KEY AUTO_INCREMENT,
     name varchar(50) NOT NULL,
@@ -25,11 +11,34 @@ CREATE TABLE IF NOT EXISTS user (
 );
 
 CREATE TABLE IF NOT EXISTS user_role (
+     id bigint PRIMARY KEY AUTO_INCREMENT,
+     user_id varchar(36) NOT NULL,
+     role_id bigint NOT NULL,
+     FOREIGN KEY (user_id) REFERENCES user(id),
+     FOREIGN KEY (role_id) REFERENCES role(id)
+);
+
+CREATE TABLE IF NOT EXISTS product (
+   id bigint PRIMARY KEY AUTO_INCREMENT,
+   name varchar(50) NOT NULL,
+   unit_price decimal(10, 2) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS sale (
     id bigint PRIMARY KEY AUTO_INCREMENT,
-    user_id VARCHAR(36) NOT NULL,
-    role_id bigint NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user(id),
-    FOREIGN KEY (role_id) REFERENCES role(id)
+    user_id varchar(36) NOT NULL,
+    total_value decimal(10, 2) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+CREATE TABLE IF NOT EXISTS sale_product (
+    id bigint PRIMARY KEY AUTO_INCREMENT,
+    sale_id bigint NOT NULL,
+    product_id bigint NOT NULL,
+    quantity int NOT NULL,
+    total_value decimal(10, 2),
+    FOREIGN KEY (sale_id) REFERENCES sale(id),
+    FOREIGN KEY (product_id) REFERENCES product(id)
 );
 
 INSERT IGNORE INTO role(id, name, description) VALUES (1, 'ADMIN', 'has all system accesses');
