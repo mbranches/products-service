9a3dffd20a68d7e0ca634a819c777a709836a8de
mbranches <marcusvbranches@gmail.com>
2025-06-05 14:25:04 -0300

    refactor: adiciona role ADMIN necessária para acessar método findAll de sales

--- src/main/java/dev/branches/config/SecurityConfig.java
+++ src/main/java/dev/branches/config/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/v1/users").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.GET, "/api/v1/users").hasRole("MANAGER")
                         .requestMatchers(HttpMethod.POST, "/api/v1/products").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/v1/sales").hasRole("MANAGER")
                         .requestMatchers(HttpMethod.POST, "/api/v1/sales").hasRole("BASIC")
                         .anyRequest().authenticated()
                 )
