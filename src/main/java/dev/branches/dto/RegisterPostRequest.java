5b710a8473bad51592576b5af9a9b9e305da8bc5
mbranches <marcusvbranches@gmail.com>
2025-06-06 10:22:52 -0300

    feat: adiciona firstName e lastName à classe dto de registro

--- src/main/java/dev/branches/dto/RegisterPostRequest.java
+++ src/main/java/dev/branches/dto/RegisterPostRequest.java
@@ -1,4 +1,4 @@
 package dev.branches.dto;
 
-public record RegisterPostRequest(String login, String password) {
+public record RegisterPostRequest(String firstName, String lastName, String login, String password) {
 }
