6a00e8011f6f5c94890ece7171b98f5919e6a8d1
mbranches <marcusvbranches@gmail.com>
2025-06-06 12:03:16 -0300

    feat: adiciona captura e tratamento da exceção MethodArgumentNotValidException

---
+++ src/main/java/dev/branches/exception/ArgumentNotValidMessageError.java
@@ -0,0 +1,3 @@
+package dev.branches.exception;
+
+public record ArgumentNotValidMessageError(int status, String error, String message) {}
--- src/main/java/dev/branches/exception/GlobalErrorHandlerAdvice.java
+++ src/main/java/dev/branches/exception/GlobalErrorHandlerAdvice.java
@@ -1,10 +1,15 @@
 package dev.branches.exception;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalErrorHandlerAdvice {
@@ -27,5 +32,19 @@ public class GlobalErrorHandlerAdvice {
         DefaultMessageError error = new DefaultMessageError(HttpStatus.UNAUTHORIZED.value(), "User or password invalid");
 
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ArgumentNotValidMessageError> handlerMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String defaultMessage = e.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .filter(Objects::nonNull)
+                .sorted()
+                .collect(Collectors.joining(", "));
+
+        ArgumentNotValidMessageError response = new ArgumentNotValidMessageError (HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase(), defaultMessage);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 }
