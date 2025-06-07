ca6022f121811cae1ff05a2b8da96c3e0595540b
mbranches <marcusvbranches@gmail.com>
2025-06-07 12:22:06 -0300

    feat: adiciona ProductMapper

---
+++ src/main/java/dev/branches/mapper/ProductMapper.java
@@ -0,0 +1,21 @@
+package dev.branches.mapper;
+
+import dev.branches.dto.request.ProductPostRequest;
+import dev.branches.dto.response.ProductGetResponse;
+import dev.branches.dto.response.ProductPostResponse;
+import dev.branches.model.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.springframework.context.annotation.Primary;
+
+import java.util.List;
+
+@Primary
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface ProductMapper {
+    List<ProductGetResponse> toProductGetResponseList(List<Product> productList);
+
+    ProductPostResponse toProductPostResponse(Product product);
+
+    Product toProduct(ProductPostRequest postRequest);
+}
