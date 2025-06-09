dfdd0c72dbc93a0e1893cca6562a297a4938efd7
mbranches <marcusvbranches@gmail.com>
2025-06-09 13:01:17 -0300

    feat: adiciona exemplos para a documentação em ArgumentNotValidMessageError

--- src/main/java/dev/branches/exception/ArgumentNotValidMessageError.java
+++ src/main/java/dev/branches/exception/ArgumentNotValidMessageError.java
@@ -1,3 +1,12 @@
 package dev.branches.exception;
 
-public record ArgumentNotValidMessageError(int status, String error, String message) {}
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record ArgumentNotValidMessageError(
+        @Schema(description = "response status code", example = "404")
+        int status,
+        @Schema(description = "response status", example = "Bad Request")
+        String error,
+        @Schema(description = "error message", example = "The field 'XX' is required")
+        String message
+) {}
